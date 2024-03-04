db3cea05ab26b1b331955275a1e32c2dd9fef9b4
hyeonmin11 <99racer@naver.com>
2024-03-04 17:40:07 +0900

    1

--- baekjoon/12865.py
+++ baekjoon/12865.py
@@ -1,1 +1,27 @@
-print("hello world")
+import sys
+
+numbers = []
+CNT, W = map(int, sys.stdin.readline().split())
+for line in sys.stdin:
+    things = line.strip().split()
+    things = [int(thing) for thing in things]
+    
+    numbers.append(tuple(things))
+
+# dp = []
+# for _ in range(CNT+1):
+#     dp.append([0]*(W+1))
+dp = [[0]*(W+1) for _ in range((CNT+1))]
+#print(dp)
+
+for i in range(1, CNT+1):
+    for j in range(1, W+1):
+        wght = numbers[i-1][0]
+        val = numbers[i-1][1]
+        
+        if j < wght:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-wght] + val)
+
+print(dp[CNT][W])
